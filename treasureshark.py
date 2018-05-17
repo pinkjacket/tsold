e6caaf416dab92dd36c1ae93e372be2662f8541b
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-05-16 22:48:15 -0700

    dungeon generator created

--- treasureshark.py
+++ treasureshark.py
@@ -1,10 +1,18 @@
 import tdl
+from random import randint
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
+
 MAP_WIDTH = 80
 MAP_HEIGHT = 45
+
+ROOM_MAX_SIZE = 18
+ROOM_MIN_SIZE = 6
+MAX_ROOMS = 30
+
 LIMIT_FPS = 20
+
 color_dark_wall = (0, 0, 100)
 color_dark_ground = (50, 50, 150)
 
@@ -27,7 +35,15 @@ class Rect:
         self.x2 = x + w
         self.y2 = y + h
 
+    def center(self):
+        center_x = (self.x1 + self.x2) // 2
+        center_y = (self.y1 + self.y2) // 2
+        return(center_x, center_y)
 
+    def intersect(self, other):
+        # true if rooms overlap
+        return(self.x1 <= other.x2 and self.x2 >= other.x1 and
+               self.y1 <= other.y2 and self.y2 >= other.y1)
 
 
 class GameObject:
@@ -55,16 +71,50 @@ def make_map():
     # fill map with blocked tiles
     my_map = [[Tile(True) for y in range(MAP_HEIGHT)] for x in range(MAP_WIDTH)]
 
-    # make two rooms
-    room1 = Rect(20, 15, 10, 15)
-    room2 = Rect(50, 15, 10, 15)
-    create_room(room1)
-    create_room(room2)
-    create_h_tunnel(25, 55, 23)
+    rooms = []
+    num_rooms = 0
 
-    # put the player in one
-    player.x = 25
-    player.y = 23
+    for r in range(MAX_ROOMS):
+        # random width and height
+        w = randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = randint(ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        # random position inside the map
+        x = randint(0, MAP_WIDTH-w-1)
+        y = randint(0, MAP_HEIGHT-h-1)
+
+        new_room = Rect(x, y, w, h)
+
+        # check for intersection with other rooms
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+
+        if not failed:
+            create_room(new_room)
+            (new_x, new_y) = new_room.center()
+
+            if num_rooms == 0:
+                # first room created, player starts here
+                player.x = new_x
+                player.y = new_y
+
+            else:
+                # rooms after the first
+                # connected to previous room by a tunnel
+
+                (prev_x, prev_y) = rooms[num_rooms-1].center()
+
+                if randint(0, 1):
+                    create_h_tunnel(prev_x, new_x, prev_y)
+                    create_v_tunnel(prev_y, new_y, new_x)
+                else:
+                    create_v_tunnel(prev_y, new_y, prev_x)
+                    create_h_tunnel(prev_x, new_x, new_y)
+
+            rooms.append(new_room)
+            num_rooms += 1
 
 
 def create_h_tunnel(x1, x2, y):
@@ -74,7 +124,7 @@ def create_h_tunnel(x1, x2, y):
         my_map[x][y].block_sight = False
 
 
-def create_v_tunnel(x1, x2, y):
+def create_v_tunnel(y1, y2, x):
     global my_map
     for y in range(min(y1, y2), max(y1, y2) + 1):
         my_map[x][y].blocked = False
