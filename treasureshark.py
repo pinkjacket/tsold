125c1e0253db9414dab85c69713ce80a8ad32bf4
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-05-16 20:45:55 -0700

    basic room creation

--- treasureshark.py
+++ treasureshark.py
@@ -19,6 +19,17 @@ class Tile:
         self.block_sight = block_sight
 
 
+class Rect:
+    # makes a rectangle for a room
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+
+
+
 class GameObject:
     def __init__(self, x, y, char, color):
         self.x = x
@@ -41,14 +52,42 @@ class GameObject:
 def make_map():
     global my_map
 
-    # fill map with unblocked tiles
-    my_map = [[Tile(False) for y in range(MAP_HEIGHT)] for x in range(MAP_WIDTH)]
+    # fill map with blocked tiles
+    my_map = [[Tile(True) for y in range(MAP_HEIGHT)] for x in range(MAP_WIDTH)]
 
-    # test pillars
-    my_map[30][22].blocked = True
-    my_map[30][22].block_sight = True
-    my_map[50][22].blocked = True
-    my_map[50][22].block_sight = True
+    # make two rooms
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(50, 15, 10, 15)
+    create_room(room1)
+    create_room(room2)
+    create_h_tunnel(25, 55, 23)
+
+    # put the player in one
+    player.x = 25
+    player.y = 23
+
+
+def create_h_tunnel(x1, x2, y):
+    global my_map
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        my_map[x][y].blocked = False
+        my_map[x][y].block_sight = False
+
+
+def create_v_tunnel(x1, x2, y):
+    global my_map
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        my_map[x][y].blocked = False
+        my_map[x][y].blocked = False
+
+
+def create_room(room):
+    global my_map
+    # make the tiles in a rectangle passable
+    for x in range(room.x1 +1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            my_map[x][y].blocked = False
+            my_map[x][y].block_sight = False
 
 
 def render_all():
